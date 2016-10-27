2bf5387894aed843c8d12955f9ef42a862403d8d
potluriP9 <potluridevops@gmail.com>
2016-10-27 15:58:38 -0400

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,1 +1,22 @@
+node '*' {  
+  class { 'apache':    
+    default_mods => false, 
+  }  
 
+  include apache::mod::ssl  
+
+  apache::vhost { '*-http':    
+    servername      => 'Potluri.puppetagent*.com',    
+    port            => '80',    
+    docroot         => '/var/www/html/',    
+    redirect_status => 'permanent',    
+    redirect_dest   => 'https://Potluri.puppetagent*.com/',  
+  }  
+
+  apache::vhost { '*-https':    
+    servername      => 'Potluri.puppetagent*.com',    
+    port            => '443',    
+    docroot         => '/var/www/html/',    
+    ssl             => true,    
+  }
+}
